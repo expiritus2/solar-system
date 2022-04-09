57adba26dce5cb048a0a14a77dd721f94320f5c6
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-04-09 23:23:30 +0300

    Init

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const ghpages = require('gh-pages');
 
 ghpages.publish('dist', {
   branch: 'master',
-  repo: 'git@github.com:expiritus2/solar-system.git',
+  repo: 'https://github.com/expiritus2/solar-system.git',
 }, (err) => {
   if(err) { console.log(err) }
   else { console.log('Deploy is successfull'); }
