e5475280fbace46b4d6d386178347d75d7eda787
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-04-09 21:55:36 +0300

    Update gh-pages version

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const ghpages = require('gh-pages');
 
 ghpages.publish('dist', {
   branch: 'master',
-  repo: 'https://github.com/expiritus2/solar-system',
+  repo: 'git@expiritus2.github.com:expiritus2/solar-system.git',
 }, (err) => {
   if(err) { console.log(err) }
   else { console.log('Deploy is successfull'); }
